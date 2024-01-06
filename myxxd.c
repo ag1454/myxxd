2d8633f43b1840c2324507758aac735471783775
ag1454 <98437414+ag1454@users.noreply.github.com>
2024-01-06 16:24:47 -0500

    Update myxxd.c

--- myxxd.c
+++ myxxd.c
@@ -1,10 +1,144 @@
 #include <stdio.h>
 #include <string.h>
 
+void myxxdP(FILE* file, int c)
+{
+        if (file)                                                       // if file exists
+        {
+                while ((c = getc(file)) != EOF)                         // go through each char in file...
+                {
+                        printf("%02x", c);                              // ...and print out hex version
+                }
+        }
+        else                                                            // if file does not exist
+        {
+                printf("A file under that name does not exist.\n");
+        }
+
+        printf("\n");
+
+        return;
+}
+
+void myxxdB(FILE* file, int c)
+{
+        if (file)                                                       // if file exists
+        {
+                while ((c = getc(file)) != EOF)                         // go through each char in file...
+                {
+                        for (int i = 0; i < 8; i++)                     // loop 8 times because a char in C is 8 bytes
+                        {
+                                printf("%d", !!((c << i) & 0x80));      // print binary of c (8 bytes)
+                        }
+
+                        printf(" ");                                    // spacing between bytes
+                }
+        }
+        else                                                            // if file does not exist
+        {
+                printf("A file under that name does not exist.\n");
+        }
+
+        printf("\n");
+
+        return;
+}
+
+void myxxdC(FILE* file, int c)
+{
+        int count = 1;                                                  // tracking for printing
+
+        if (file)                                                       // if file exists
+        {
+                while ((c = getc(file)) != EOF)                         // go through each char in file...
+                {
+                        printf("%02x", c);
+                        count++;                                        // increment
+
+                        if (count == 3)                                 // for spacing between hex
+                        {
+                                printf(" ");                            // space
+                                count = 1;                              // reset
+                        }
+                }
+        }
+        else                                                            // if file does not exist
+        {
+                printf("A file under that name does not exist.\n");
+        }
+
+        printf("\n");
+
+        return;
+}
+
+void myxxdE(FILE* file, int c)
+{
+        int endian[135];                                                // storage for -e
+        int i = 0;                                                      // index
+
+        if (file)                                                       // if file exists
+        {
+                while ((c = getc(file)) != EOF)                         // go through each char in file...
+                {
+                        endian[i] = c & 255;                            // put hex version in endian[]
+                        i++;
+                }
+
+                for (int i = 0; i < 132; i += 4)                        // convert to hex and print out little endian format
+                {
+                        printf("%02x", endian[i + 3] & 255);
+                        printf("%02x", endian[i + 2] & 255);
+                        printf("%02x", endian[i + 1] & 255);
+                        printf("%02x", endian[i] & 255);
+                        printf(" ");
+                }
+
+                printf("%02x", endian[134] & 255);                      // the rest
+                printf("%02x", endian[133] & 255);
+                printf("%02x", endian[132] & 255);
+        }
+        else                                                            // if file does not exist
+        {
+                printf("A file under that name does not exist.\n");
+        }
+
+        printf("\n");
+
+        return;
+}
+
+void myxxdU(FILE* file, int c)
+{
+        int count = 1;                                                  // tracking for printing
+
+        if (file)                                                       // if file exists
+        {
+                while ((c = getc(file)) != EOF)                         // go through each char in file...
+                {
+                        printf("%02X", c);                              // capital x for capital hex
+                        count++;                                        // increment
+
+                        if (count == 3)                                 // for spacing between hex
+                        {
+                                printf(" ");                            // space
+                                count = 1;                              // reset
+                        }
+                }
+        }
+        else                                                            // if file does not exist
+        {
+                printf("A file under that name does not exist.\n");
+        }
+
+        printf("\n");
+
+        return;
+}
+
 int main(int argc, char *argv[])
 {
         int c;                                                                  // store file contents
-        int endian[135];                                                        // storage for -e
 
         if (argc != 3)                                                          // if user inputs too many or too few arguments...
         {
@@ -16,121 +150,23 @@ int main(int argc, char *argv[])
 
         if (strcmp(argv[1], "-p") == 0)                                         // if user inputs -p flag
         {
-                if (file)                                                       // if file exists
-                {
-                        while ((c = getc(file)) != EOF)                         // go through each char in file...
-                        {
-                                printf("%02x", c);                              // ...and print out hex version
-                        }
-                }
-                else                                                            // if file does not exist
-                {
-                        printf("A file under that name does not exist.\n");
-                }
-
-                printf("\n");
+                myxxdP(file, c);
         }
         else if (strcmp(argv[1], "-b") == 0)                                    // if user inputs -b flag
         {
-                if (file)                                                       // if file exists
-                {
-                        while ((c = getc(file)) != EOF)                         // go through each char in file...
-                        {
-                                for (int i = 0; i < 8; i++)                     // loop 8 times because a char in C is 8 bytes
-                                {
-                                        printf("%d", !!((c << i) & 0x80));      // print binary of c (8 bytes)
-                                }
-
-                                printf(" ");                                    // spacing between bytes
-                        }
-                }
-                else                                                            // if file does not exist
-                {
-                        printf("A file under that name does not exist.\n");
-                }
-
-                printf("\n");
+                myxxdB(file, c);
         }
         else if (strcmp(argv[1], "-C") == 0)                                    // if user inputs -C flag
         {
-                int count = 1;                                                  // tracking for printing
-
-                if (file)                                                       // if file exists
-                {
-                        while ((c = getc(file)) != EOF)                         // go through each char in file...
-                        {
-                                printf("%02x", c);
-                                count++;                                        // increment
-
-                                if (count == 3)                                 // for spacing between hex
-                                {
-                                        printf(" ");                            // space
-                                        count = 1;                              // reset
-                                }
-                        }
-                }
-                else                                                            // if file does not exist
-                {
-                        printf("A file under that name does not exist.\n");
-                }
-
-                printf("\n");
+                myxxdC(file, c);
         }
         else if (strcmp(argv[1], "-e") == 0)                                    // if user inputs -e flag
         {
-                int i = 0;                                                      // index
-
-                if (file)                                                       // if file exists
-                {
-                        while ((c = getc(file)) != EOF)                         // go through each char in file...
-                        {
-                                endian[i] = c & 255;                            // put hex version in endian[]
-                                i++;
-                        }
-
-                        for (int i = 0; i < 132; i += 4)                        // convert to hex and print out little endian format
-                        {
-                                printf("%02x", endian[i + 3] & 255);
-                                printf("%02x", endian[i + 2] & 255);
-                                printf("%02x", endian[i + 1] & 255);
-                                printf("%02x", endian[i] & 255);
-                                printf(" ");
-                        }
-
-                        printf("%02x", endian[134] & 255);                      // the rest
-                        printf("%02x", endian[133] & 255);
-                        printf("%02x", endian[132] & 255);
-                }
-                else                                                            // if file does not exist
-                {
-                        printf("A file under that name does not exist.\n");
-                }
-
-                printf("\n");
+                myxxdE(file, c);
         }
         else if (strcmp(argv[1], "-u") == 0)                                    // if user inputs -u flag
         {
-                int count = 1;                                                  // tracking for printing
-
-                if (file)                                                       // if file exists
-                {
-                        while ((c = getc(file)) != EOF)                         // go through each char in file...
-                        {
-                                printf("%02X", c);                              // capital x for capital hex
-                                count++;                                        // increment
-
-                                if (count == 3)                                 // for spacing between hex
-                                {
-                                        printf(" ");                            // space
-                                        count = 1;                              // reset
-                                }
-                        }
-                }
-                else                                                            // if file does not exist
-                {
-                        printf("A file under that name does not exist.\n");
-                }
-                printf("\n");
+                myxxdU(file, c);
         }
         else                                                                    // user inputs invalid flags
         {
